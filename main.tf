c655da17f38573e922fc11187e8186245db060ee
Ronnie Mitra <ronnie.mitra@gmail.com>
2020-09-27 18:41:03 +0100

    initial network release

--- main.tf
+++ main.tf
@@ -11,3 +11,25 @@ locals {
   aws_region       = "eu-west-2"
   k8s_cluster_name = "ms-cluster"
 }
+
+# Network Configuration
+
+module "aws-network" {
+  source = "github.com/mitraman/module-aws-network"
+
+  env_name              = local.env_name
+  vpc_name              = "msur-VPC"
+  cluster_name          = local.k8s_cluster_name
+  aws_region            = local.aws_region
+  main_vpc_cidr         = "10.10.0.0/16"
+  public_subnet_a_cidr  = "10.10.0.0/18"
+  public_subnet_b_cidr  = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
+
+
+# EKS Configuration 
+
+# GitOps Configuration
+
